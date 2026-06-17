5cca538d8ddb9a73b46083b847651e1ab32d8534
Prince-24-T <pt023150@gmail.com>
2026-06-17 23:16:36 +0530

    chunks error fixed

--- server/src/services/chunkService.js
+++ server/src/services/chunkService.js
@@ -3,6 +3,9 @@ const path = require("path");
 const { Parser } = require("json2csv");
 
 function createChunks(records, chunkSize = 1000) {
+  console.log("Creating chunks...");
+  console.log("Total Records:", records.length);
+
   const chunkDir = path.join(__dirname, "../chunks");
 
   if (!fs.existsSync(chunkDir)) {
@@ -14,12 +17,18 @@ function createChunks(records, chunkSize = 1000) {
   for (let i = 0; i < records.length; i += chunkSize) {
     const chunk = records.slice(i, i + chunkSize);
 
+    console.log(
+      `Creating chunk ${i / chunkSize + 1} with ${chunk.length} records`,
+    );
+
     const parser = new Parser();
 
     const csv = parser.parse(chunk);
 
     fs.writeFileSync(`${chunkDir}/chunk_${i / chunkSize + 1}.csv`, csv);
   }
+
+  console.log("Chunking completed");
 }
 
 module.exports = createChunks;
