9bcd489b744f3ce73e0e5c80285d1be1ad043944
Prince-24-T <pt023150@gmail.com>
2026-06-17 22:40:16 +0530

    upload csv error

--- server/src/routes/uploadRoutes.js
+++ server/src/routes/uploadRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const fs = require("fs");
 const multer = require("multer");
+const path = require("path");
 const { processCSV } = require("../controllers/uploadController");
 
 const router = express.Router();
+const uploadPath = path.join(__dirname, "../uploads");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "src/uploads");
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath);
   },
 
   filename: function (req, file, cb) {
